6709722faf3c78bae22427afc55c4e011177b6c9
Joseph Kunz <joseph.kunz@gmx.de>
2019-12-21 12:00:47 +0100

    adds new and create action for gift controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -6,4 +6,23 @@ class GiftsController < ApplicationController
   def show
     @gift = Gift.find(params[:id])
   end
+
+  def new
+    @gift = Gift.new
+  end
+
+  def create
+    @gift = Gift.new(gift_params)
+    if @gift.save
+      redirect_to gift_path(@gift)
+  end
+
+end
+
+
+
+private
+
+def gift_params
+  params.require(:gift).permit(:id
 end
